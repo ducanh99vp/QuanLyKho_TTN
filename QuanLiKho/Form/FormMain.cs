5d2283b792f1060acaeb0ec4fd1ffda96a9e69f2
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-25 20:19:35 +0700

    DA_Khoi tao Form Main va CSDL

---
+++ QuanLiKho/Form/FormMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Windows.Forms.Layout;
+using DevExpress.XtraEditors;
+using System.Data;
+using System.Threading;
+using System.Data.SqlClient;
+using DevExpress.Charts;
+using System.Configuration;
+
+namespace QuanLiKho
+{
+    public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormMain()
+        {
+            InitializeComponent();        
+        }
+    }
+}
