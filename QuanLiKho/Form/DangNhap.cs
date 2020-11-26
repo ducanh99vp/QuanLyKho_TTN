26c52cb51799ee6cf92c943d770795688a99cdd9
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-26 09:33:09 +0700

    BT_Tao form dang nhap

---
+++ QuanLiKho/Form/DangNhap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraEditors;
+
+
+
+
+namespace QuanLiKho
+{
+    
+    public partial class DangNhap : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        KetNoiCSDL con = new KetNoiCSDL();
+
+        
+        //name ủe
+        private string lbNameUser = "Quyền ";
+
+        public DangNhap()
+        {
+            InitializeComponent();
+
+            
+
+          
+        }
+
+      
+
+ 
+    }
+}
