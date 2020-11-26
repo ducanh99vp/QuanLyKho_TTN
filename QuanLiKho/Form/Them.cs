b54b29178399d3efd433e7964b7776aa5a5c0c18
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-26 08:27:52 +0700

    TQ_Them_Tao giao dien trang them KH NPP KhoH DVi NhomH HH

---
+++ QuanLiKho/Form/Them.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using System.Data.SqlClient;
+using DevExpress.XtraEditors;
+namespace QuanLiKho
+{
+    public partial class Them : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+      
+        public Them()
+        {
+            InitializeComponent();
+
+            
+        }
+        public Them(string strInit)
+        {
+            InitializeComponent();
+            stateEvent = strInit;
+
+            init(strInit);
+            initPQ();
+        }
+
+       
+        
+    }
+}
