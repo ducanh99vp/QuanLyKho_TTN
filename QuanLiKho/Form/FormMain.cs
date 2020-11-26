a8c826b27646eeae3cada38e2e8c42610569c124
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-26 13:53:09 +0700

    TL_ Thiet ke form nhap kho- su kien click hien thi form

--- QuanLiKho/Form/FormMain.cs
+++ QuanLiKho/Form/FormMain.cs
@@ -20,5 +20,85 @@ namespace QuanLiKho
         {
             InitializeComponent();        
         }
+
+        ///////// TAB KHO HANG
+        // TAB NHAP KHO
+        private void barBtnNhapKho_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            //them tab
+            AddXtraTab(xtraTPNhapKho);
+
+            if (!(txtMaNK.Text == "" || txtDienThoaiNK.Text == "" || comboBoxEditTenNK.Text == "" || txtMaXK.Text == "" || txtDiaChiNK.Text == ""))
+            {
+                txtMaNPP.Enabled = false;
+                txtGhiChuNK.Enabled = false;
+                txtDiaChiNK.Enabled = false;
+                txtDienThoaiNK.Enabled = false;
+                comboBoxEditTenNK.Enabled = false;
+                txtMaNK.Enabled = false;
+                dateEditNK.Enabled = false;
+                txtMSTNK.Enabled = false;
+
+                btnCancelNK.Enabled = false;
+                btnOKNK.Enabled = false;
+                btnRefeshNK.Enabled = false;
+            }
+
+
+            //nhat ki 
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Nhập Kho',N'Xem','" +
+                string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+
+            //MessageBox.Show(string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime));
+        }
+        private void btnNewNK_Click(object sender, EventArgs e)
+        {
+            txtMaNPP.Enabled = true;
+            txtGhiChuNK.Enabled = true;
+            txtDiaChiNK.Enabled = true;
+            txtDienThoaiNK.Enabled = true;
+            comboBoxEditTenNK.Enabled = true;
+            txtMaNK.Enabled = true;
+            dateEditNK.Enabled = true;
+            txtMSTNK.Enabled = true;
+
+            btnRefeshNK.Enabled = true;
+
+            txtMaNPP.Text = "";
+            txtGhiChuNK.Text = "";
+            txtDiaChiNK.Text = "";
+            txtDienThoaiNK.Text = "";
+            txtMaNK.Text = "";
+            txtMSTNK.Text = "";
+            comboBoxEditTenNK.Text = "";
+
+            DataTable temp0 = new DataTable();
+            temp0 = con.GetDataTable("select * from tblNPP");
+            comboBoxEditTenNK.Properties.DataSource = temp0;
+
+
+            DataTable temp = new DataTable();
+            temp = con.GetDataTable("select * from tblNhapKhoTemp as a join tblHangHoa as b on a.HHMa=b.HHMa");
+            gridControlNK.DataSource = temp;
+
+            DataTable temp1 = new DataTable();
+            temp1 = con.GetDataTable("select * from tblNhapKho");
+            if (temp1.Rows.Count > 0)
+            {
+                //lay ma nhap kho
+                string tempStr = temp1.Rows[temp1.Rows.Count - 1][3].ToString();
+
+                int i = Convert.ToInt32(tempStr.Substring(2));
+                i++;
+                txtMaNK.Text = "NK" + i.ToString("0000");
+
+
+            }
+            else txtMaNK.Text = "NK0001";
+
+            dateEditNK.Text = DateTime.Today.ToString();
+        }
+
     }
 }
