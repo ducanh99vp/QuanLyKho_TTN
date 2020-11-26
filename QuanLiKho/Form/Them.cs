94c09aa86df134f5f202dab8b8d5c0881237c87a
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-11-26 23:26:57 +0700

    TQ_Them_Thiet ke ban dau cho giao dien chuc nang them cac muc

--- QuanLiKho/Form/Them.cs
+++ QuanLiKho/Form/Them.cs
@@ -14,12 +14,15 @@ namespace QuanLiKho
 {
     public partial class Them : DevExpress.XtraBars.Ribbon.RibbonForm
     {
-      
+        private KetNoiCSDL con = new KetNoiCSDL();
+
+        private string stateEvent;
+        private string lbNameUser = "Quyền ";
         public Them()
         {
             InitializeComponent();
+            lbNameUser += con.GetValue("select name from tblLuuMK where num='1'", 0);
 
-            
         }
         public Them(string strInit)
         {
@@ -29,8 +32,62 @@ namespace QuanLiKho
             init(strInit);
             initPQ();
         }
+        private void init(string state)
+        {
+            //ban dau
+            btnThem.Enabled = true;
+            btnCancel.Enabled = true;
 
-       
-        
+            btnXoa.Enabled = false;
+            btnXoaToanBo.Enabled = false;
+            btnCapNhat.Enabled = false;
+            btnSua.Enabled = false;
+
+            txtMa.ReadOnly = true;
+            //thiet lap 
+        }
+        private void XtraTabMain_Click(object sender, EventArgs e)
+        {
+            //thiet lap ban dau
+            if (XtraTabMain.SelectedTabPage == LayerThem)
+            {
+                btnThem.Enabled = true;
+                btnCancel.Enabled = true;
+
+                btnXoa.Enabled = false;
+                btnXoaToanBo.Enabled = false;
+                btnCapNhat.Enabled = false;
+                btnSua.Enabled = false;
+            }
+            else if (XtraTabMain.SelectedTabPage == LayerXoa)
+            {
+                btnThem.Enabled = false;
+                btnCancel.Enabled = false;
+
+                btnXoa.Enabled = true;
+                btnXoaToanBo.Enabled = true;
+                btnCapNhat.Enabled = false;
+                btnSua.Enabled = false;
+            }
+            else if (XtraTabMain.SelectedTabPage == LayerCapNhat)
+            {
+                btnThem.Enabled = false;
+                btnCancel.Enabled = false;
+
+                btnXoa.Enabled = false;
+                btnXoaToanBo.Enabled = false;
+                btnCapNhat.Enabled = true;
+                btnSua.Enabled = true;
+            }
+            //=====================================================
+            private void btnThem_ItemClick(object sender, ItemClickEventArgs e)
+            {
+                ThemDL(stateEvent);
+                this.Dispose();
+            }
+        }
+
+
+
     }
-}
+    }
