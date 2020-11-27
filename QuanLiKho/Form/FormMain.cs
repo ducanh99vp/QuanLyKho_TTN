b433889bd84687efb280d6523aa0b0cf038f63b8
anh duong duc <ducanh99tpvpnd@gmail.com>
2020-11-27 10:37:16 +0700

    DA_Xu ly cac Tab trong FormMain

--- QuanLiKho/Form/FormMain.cs
+++ QuanLiKho/Form/FormMain.cs
@@ -14,8 +14,56 @@ using System.Configuration;
 
 namespace QuanLiKho
 {
+    public enum state
+    {
+        StateNK,
+        StateXK,
+        StateTK
+    }
     public partial class FormMain : DevExpress.XtraBars.Ribbon.RibbonForm
     {
+        public static state StateManager;
+        KetNoiCSDL con = new KetNoiCSDL();
+
+        //dem so luong tab con hien thi
+        private int vSoXtraTab = 1;
+
+        //khai bao cac thu tuc xuat kho
+        SQL_tblXuatKho sqlXK = new SQL_tblXuatKho();
+        EC_tblXuatKho ecXK = new EC_tblXuatKho();
+
+
+
+        //khai bao cac thu tuc khach hang
+        SQL_tblKhachHang sqlKH = new SQL_tblKhachHang();
+
+        //dang nhap, dang xuat
+        private bool active = false;
+
+
+        //lay dong chon trong bang phan quyen
+        private string userSelect;
+
+        //dung cho bo loc bao cao
+        private string stateBC;
+
+        //phan quyen
+        private bool xoaDL = true;
+
+        //Thoi gian bieu do
+        private string start;
+        private string end;
+
+        //nhap kho
+        private string dateState = "tuan";
+        private string cmdTime;
+
+        private string start1;
+        private string end1;
+
+        //Xuat kho
+        private string cmdTime1;
+        private string stateQL;
         public FormMain()
         {
             InitializeComponent();        
