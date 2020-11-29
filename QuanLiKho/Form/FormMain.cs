19fec11b5acf0b25ba5a1285774692bba0441958
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-11-29 09:42:18 +0700

    TL_ Bat su kien cancle xuat kho va hoan thanh form

--- QuanLiKho/Form/FormMain.cs
+++ QuanLiKho/Form/FormMain.cs
@@ -212,5 +212,86 @@ namespace QuanLiKho
             i++;
             txtMaXK.Text = "XK" + i.ToString("0000");
         }
+        private void repositoryItemGridLookUpEdit1_Leave_1(object sender, EventArgs e)
+        {
+            string getHHMa = gridView6.GetRowCellDisplayText(gridView6.FocusedRowHandle, "HHMa");
+            DataTable temp = new DataTable();
+            try
+            {
+                temp = con.GetDataTable("select * from tblHangHoa where HHMa=N'" + getHHMa + "'");
+            }
+            catch
+            {
+                XtraMessageBox.Show("Mã xuất kho chưa đúng");
+                return;
+            }
+
+            if (temp.Rows.Count != 0)
+            {
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "HHTen", temp.Rows[0][1].ToString().Trim());
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "KMa", temp.Rows[0][4].ToString().Trim());
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "DVMa", temp.Rows[0][3].ToString().Trim());
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "XKMa", txtMaXK.Text);
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "XKNgay", dateEditNgayXK.Text);
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "KHMa", gridLookUpEditMaXK.Text);
+                txtGhiChuXK.Text = temp.Rows[0][4].ToString().Trim();
+            }
+
+            if (txtMaXK.Text != "")
+            {
+                btnOKXK.Enabled = true;
+
+            }
+            else
+            {
+                btnOKXK.Enabled = false;
+            }
+        }
+        private void XKGiaEdit_Leave(object sender, EventArgs e)
+        {
+            string gia = gridView6.GetRowCellDisplayText(gridView6.FocusedRowHandle, "XKGia").Trim();
+            string soluong = gridView6.GetRowCellDisplayText(gridView6.FocusedRowHandle, "XKSL").Trim();
+
+            try
+            {
+                string tt = (int.Parse(soluong) * float.Parse(gia)).ToString();
+                gridView6.SetRowCellValue(gridView6.FocusedRowHandle, "XKThanhTien", tt);
+            }
+            catch (Exception ex)
+            {
+                XtraMessageBox.Show("Erro: " + ex.Message, "Thông Báo");
+            }
+        }
+
+        private void btnCancelXK_Click(object sender, EventArgs e)
+        {
+            if (gridControlXK.Enabled == true)
+            {
+                string MaXK = gridView6.GetRowCellDisplayText(gridView6.FocusedRowHandle, "XKMa").Trim();
+
+                try
+                {
+                    con.ThucThiCauLenhSQL("delete from tblXuatKho where XKMa='" + MaXK + "'");
+                    XtraMessageBox.Show("Đã xóa");
+                    //btnRefeshXK_Click(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    XtraMessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+
+        }
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            ExportToExcel temp = new ExportToExcel();
+            temp.exportFile("*.pdf", gridControlXK);
+        }
+
+        private void simpleButton7_Click(object sender, EventArgs e)
+        {
+            ExportToExcel temp = new ExportToExcel();
+            temp.exportFile("*.xls", gridControlXK);
+        }
     }
 }
