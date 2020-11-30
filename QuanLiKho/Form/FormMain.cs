0b3a98b0affd5a87d5629a27f8befec591182f04
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-11-30 20:23:50 +0700

    BT_Tao nut them sua xoa, chon quyen

--- QuanLiKho/Form/FormMain.cs
+++ QuanLiKho/Form/FormMain.cs
@@ -20,5 +20,93 @@ namespace QuanLiKho
         {
             InitializeComponent();        
         }
+        // TAB PHÂN QUYỀN
+        private void btnPhanQuyen_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            AddXtraTab(xtraTPPhanQuyen);
+            gridControlTTTK.DataSource = con.GetDataTable("select * from tblPhanQuyen");
+
+            //nhat ki
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Phân Quyền',N'Xem','" +
+               string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+
+        }
+        private void btnThemTK_Click(object sender, EventArgs e)
+        {
+
+            if (gridView9.OptionsView.NewItemRowPosition == DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.None)
+            {
+                gridView9.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Top;
+            }
+            else
+            {
+                SqlDataAdapter sql = con.GetCmd("select * from tblPhanQuyen");
+
+                DataTable temp = gridControlTTTK.DataSource as DataTable;
+
+                try
+                {
+                    sql.Update(temp);
+
+                    XtraMessageBox.Show("                   Đã thiết lập\nNếu thay đổi tài khoản đang hoạt động\n         vui lòng đăng nhập lại", "Thông Báo");
+                }
+                catch
+                {
+                    XtraMessageBox.Show("Nhập thiếu");
+                    return;
+                }
+                //nhat ki
+                DateTime currentTime = DateTime.Now;
+                con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Phân Quyền',N'Sửa','" +
+                   string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+            }
+        }
+        private void btnSuaTK_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter sql = con.GetCmd("select * from tblPhanQuyen");
+
+            DataTable temp = gridControlTTTK.DataSource as DataTable;
+
+            try
+            {
+                sql.Update(temp);
+
+                XtraMessageBox.Show("                   Đã thiết lập\nNếu thay đổi tài khoản đang hoạt động\n         vui lòng đăng nhập lại", "Thông Báo");
+            }
+            catch
+            {
+                XtraMessageBox.Show("Nhập thiếu");
+                return;
+            }
+            //nhat ki
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Phân Quyền',N'Sửa','" +
+               string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+        }
+        private void btnXoaTK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.ThucThiCauLenhSQL("delete tblPhanQuyen where Username='" + userSelect + "'");
+
+            }
+            catch (Exception ex)
+            {
+                XtraMessageBox.Show("Erro: " + ex.Message, "Cảnh Báo");
+                return;
+            }
+            XtraMessageBox.Show("Đã xóa", "Thông Báo");
+            //nhat ki
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Phân Quyền',N'Xóa','" +
+               string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+
+            gridControlTTTK.DataSource = con.GetDataTable("select * from tblPhanQuyen");
+        }
+        private void gridView9_RowClick(object sender, DevExpress.XtraGrid.Views.Grid.RowClickEventArgs e)
+        {
+            userSelect = gridView9.GetRowCellDisplayText(e.RowHandle, "Username").ToString();
+        }
     }
 }
