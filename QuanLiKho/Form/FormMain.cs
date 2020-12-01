184a113cfc4bc512e96f8d4361bedc57ef6e31ed
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-01 10:27:16 +0700

    BT_Tao tab nhat ky he thong

--- QuanLiKho/Form/FormMain.cs
+++ QuanLiKho/Form/FormMain.cs
@@ -20,5 +20,55 @@ namespace QuanLiKho
         {
             InitializeComponent();        
         }
+        // TAB NHẬT KÝ HỆ THỐNG
+        private void barButtonItem3_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+            //nhat ki
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Nhật Kí HT',N'Xem','" +
+               string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+
+
+            AddXtraTab(xtraTPNhatKi);
+
+            gridControlNhatKi.DataSource = con.GetDataTable("select * from tblNhatKi");
+
+
+        }
+        private void btnXoaNhatKi_Click(object sender, EventArgs e)
+        {
+            con.ThucThiCauLenhSQL("delete from tblNhatKi");
+
+            DateTime currentTime = DateTime.Now;
+            con.ThucThiCauLenhSQL("insert into tblNhatKi (NKTen,NKTacVu,NKNgay,NKUser) values (N'Nhật Kí HT',N'Xóa','" +
+                string.Format("{0:yyyy/MM/dd HH:mm:ss}", currentTime) + "',N'" + lbNameUser.Text + "')");
+
+            gridControlNhatKi.DataSource = ("select * from tblNhatKi");
+        }
+
+
+
+
+
+
+        private void xtraTPNhapKho_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupControl2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void HHTenEdit_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gridView2_CellValueChanged(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+
+        }
     }
 }
